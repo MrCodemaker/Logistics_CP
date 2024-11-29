1f6f4b9027daeb3b55cf65205f051c85756b0e92
Alexey Borodin <30794466+MrCodemaker@users.noreply.github.com>
2024-11-29 12:32:04 +0400

    29.11.2024

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="bg-blue-500 p-4 text-white text-center">
+            <h1 className="text-3xl font-bold">Логистика КП</h1>
+        </header>
+    );
+
+};
+
+export default Header;
